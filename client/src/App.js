4730a94d1418f1e277d74808fcd0e3bc139bc382
Lhoussaine bouaik <bouaik.lhou@gmail.com>
2022-12-23 01:25:20 +0100

    update posts in client

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 function App() {
   const [posts, setPosts] = useState([]);
   const [createForm, setCreateForm] = useState({ title: "", body: '' });
+  const [updateForm, setUpdateForm] = useState({ _id: null, title: "", body: '' });
 
   const fetchNotes = async () => {
     const res = await axios.get("http://localhost:3000/posts");
@@ -15,6 +16,16 @@ function App() {
     setCreateForm({ ...createForm, [name]: value })
   }
 
+
+  const handleUpdateFieldChange = (e) => {
+    const { name, value } = e.target
+    setUpdateForm({ ...updateForm, [name]: value })
+  }
+
+  const toggleUpdate = (post) => {
+    setUpdateForm({ title: post.title, body: post.body, _id: post._id })
+  }
+
   const createPost = async (e) => {
     e.preventDefault()
 
@@ -24,6 +35,20 @@ function App() {
     setPosts([...posts, res.data.post])
 
     setCreateForm({ title: "", body: '' })
+  }
+
+  const updatePost = async (e) => {
+    e.preventDefault()
+
+
+    const res = await axios.put(`http://localhost:3000/posts/${updateForm._id}`, updateForm);
+    const postIndex = posts.findIndex(post => post._id === updateForm._id)
+
+    posts[postIndex] = res.data.post
+
+    setPosts(posts)
+
+    setUpdateForm({ title: "", body: '', _id: null })
   }
 
 
@@ -48,21 +73,33 @@ function App() {
             return (
               <div key={post._id}>
                 <h3>{post.title}</h3>
+                <button onClick={() => toggleUpdate(post)}>Update</button>
                 <button onClick={() => deletePost(post._id)}>Delete</button>
               </div>
             );
           })}
       </div>
 
-      {/* Create note */}
-      <div>
+      {/* update post */}
+
+      {updateForm._id && <div>
+        <h2>update note</h2>
+        <form onSubmit={updatePost}>
+          <input value={updateForm.title} name='title' placeholder="title" onChange={handleUpdateFieldChange} />
+          <textarea value={updateForm.body} name="body" placeholder="body" onChange={handleUpdateFieldChange} />
+          <input type="submit" />
+        </form>
+      </div>}
+
+      {/* Create post */}
+      {!updateForm._id && <div>
         <h2>Create note</h2>
         <form onSubmit={createPost}>
           <input value={createForm.title} name='title' placeholder="title" onChange={updateCreateForm} />
           <textarea value={createForm.body} name="body" placeholder="body" onChange={updateCreateForm} />
           <input type="submit" />
         </form>
-      </div>
+      </div>}
     </div>
   );
 }

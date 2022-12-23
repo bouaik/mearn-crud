55efe3311e3a4979badc15264fd266ab143865a0
Lhoussaine bouaik <bouaik.lhou@gmail.com>
2022-12-23 23:40:32 +0100

    dockerize app

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import "./index.css";
 import App from "./components/App";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "http://localhost:4000";
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
